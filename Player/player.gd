0beee99b7e9524d6acae081d9e28343551b6d4a2
Kristoffer Warnberg <kristoffer.warnberg@gmail.com>
2024-05-10 21:41:38 +0200

    Fix interactables and animations

--- Player/player.gd
+++ Player/player.gd
@@ -1,21 +1,26 @@
 extends CharacterBody3D
 @onready var navigationAgent = $NavigationAgent3D
-@onready var camera = $CameraController/CameraTarget/Camera
 @onready var camera_controller = $CameraController
+@onready var camera = $CameraController/CameraTarget/Camera
+@onready var animation_tree = $AnimationTree
+@onready var animation_player = $AnimationPlayer
 
 const SPEED = 5
-const ROTATION_SPEED = 0.20
+const ROTATION_SPEED = 0.09
+
+func _ready():
+	animation_tree.active = true
 
 func _process(delta):
-	
 	if navigationAgent.is_navigation_finished():
+		animation_player.play("StretchIdle")
 		return
 	
 	moveToPoint(delta, SPEED)
-	
-	camera_controller.position = lerp(camera_controller.position, position, .8 * delta)
+	camera_controller.position = lerp(camera_controller.position, position, .8)
 
 func moveToPoint(_delta, speed):
+	animation_player.play("Walking1")
 	var targetPos = navigationAgent.get_next_path_position()
 	var direction = global_position.direction_to(targetPos)
 	faceDirection(targetPos)
@@ -25,18 +30,20 @@ func moveToPoint(_delta, speed):
 func faceDirection(_direction):
 	rotation.y=lerp_angle(rotation.y,atan2(-velocity.x,-velocity.z), ROTATION_SPEED)
 
+func shoot_ray():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_length = 1000
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.from = from
+	ray_query.to = to
+	var raycast_results = space.intersect_ray(ray_query)
+	#print(raycast_results)
+	navigationAgent.target_position = raycast_results.position
+
 func _input(_event):
 	if Input.is_action_just_pressed("LeftMouse"):
-		var mousePos = get_viewport().get_mouse_position()
-		var rayLength = 100
-		var from = camera.project_ray_origin(mousePos)
-		var to = from + camera.project_ray_normal(mousePos) * rayLength
-		var space = get_world_3d().direct_space_state
-		var rayQuery = PhysicsRayQueryParameters3D.new()
-		rayQuery.from = from
-		rayQuery.to = to
-		rayQuery.collide_with_areas = true
-		var result = space.intersect_ray(rayQuery)
-		print(result)
-
-		navigationAgent.target_position = result.position
+		shoot_ray()
+	
--- Systems/interactable_dialogue.gd
+++ Systems/interactable_dialogue.gd
@@ -1,12 +1,32 @@
 extends Area3D
+@onready var timer = $Timer
 
+@export var id: String
+var can_click = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
 
+func _process(_delta):
+	if can_click:
+		print("You can click now")
+		if Input.is_action_just_pressed("RightMouse"):
+			print("You clicked on: " + id)
+
 func _on_body_entered(_body):
+	$AnimationPlayer.play("appear")
 	visible = true
 
 func _on_body_exited(_body):
+	$AnimationPlayer.play("disappear")
+	timer.start()
+
+func _on_click_area_mouse_entered():
+	print("Hello there")
+	can_click = true
+
+func _on_click_area_mouse_exited():
+	can_click = false
+
+func _on_timer_timeout():
 	visible = false
