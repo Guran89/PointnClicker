2116ed2e3256b33f8384019cbd2bfc9a1a239861
Kristoffer Warnberg <kristoffer.warnberg@gmail.com>
2024-05-15 07:56:27 +0200

    Room appear animation

--- Systems/NavigationRegion.gd
+++ Systems/NavigationRegion.gd
@@ -1,14 +1,18 @@
 extends NavigationRegion3D
 @onready var timer = $Timer
 @onready var navigation_link = $NavigationLink
+@onready var room_2 = $Room2
+@onready var animation_player = $Room2/AnimationPlayer
 
 func _ready():
 	navigation_link.enabled = false
+	room_2.visible = false
 
 
 func _on_door_door_opened():
 	navigation_link.enabled = true
 	timer.start()
+	animation_player.play("appear")
 
 
 func _on_timer_timeout():
